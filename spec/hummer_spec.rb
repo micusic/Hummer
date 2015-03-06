091c418734d73e867ef0e194ef43b9155dd4e144
Micus Zhang <micus.zhang@gmail.com>
2015-03-06 16:00:09 +0800

    use phantomjs

--- spec/hummer_spec.rb
+++ spec/hummer_spec.rb
@@ -3,21 +3,22 @@ require 'selenium-webdriver'
 
 describe "Google" do
   it "changes title with keyword" do
-	driver = Selenium::WebDriver.for :chrome
-	driver.get "http://google.com"
+	driver = Selenium::WebDriver.for :phantomjs
+	driver.get "http://www.google.com"
+
+	puts "Page title is #{driver.title}"
+    expect(driver.title.downcase).not_to include('cheese!')
 
 	element = driver.find_element :name => "q"
 	element.send_keys "Cheese!"
 	element.submit
 
-	puts "Page title is #{driver.title}"
-
 	wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 	wait.until { driver.title.downcase.start_with? "cheese!" }
 
+	puts "Page title is #{driver.title}"
     expect(driver.title.downcase).to include('cheese!')
 
-	puts "Page title is #{driver.title}"
 	driver.quit
   end
 end
