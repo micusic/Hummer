dd054d8cb5631d1c0c557363bd9be8aab84997c4
Micus Zhang <micus.zhang@gmail.com>
2015-04-08 10:09:12 +0800

    get key info and put in console.

--- spec/hummer_spec.rb
+++ spec/hummer_spec.rb
@@ -1,25 +1,66 @@
+#encoding:UTF-8
 require 'rubygems'
 require 'selenium-webdriver'
 
-describe "Google" do
-  it "changes title with keyword" do
-	driver = Selenium::WebDriver.for :phantomjs
-	driver.get "http://www.google.com"
+describe '51Job' do
+  it 'get latest resumes' do
+    driver = Selenium::WebDriver.for :chrome
+    driver.get 'http://ehire.51job.com/MainLogin.aspx'
+    expect(driver.title.downcase).to eq('人才招聘-前程无忧 | 51job 网才')
+    puts 'opened 51Job.'
 
-	puts "Page title is #{driver.title}"
-    expect(driver.title.downcase).not_to include('cheese!')
+    driver.find_element(:id, 'txtMemberNameCN').send_keys('thoughtworks')
+    driver.find_element(:id, 'txtUserNameCN').send_keys('thoughtworks')
+    driver.find_element(:id, 'txtPasswordCN').send_keys('tw2015!')
+    driver.find_element(:id, 'Login_btnLoginCN').click
+    puts 'logged in'
+    if driver.title.downcase == '在线用户管理'
+      driver.find_element(:css, 'a').click
+      puts 'kicked user'
+    end
 
-	element = driver.find_element :name => "q"
-	element.send_keys "Cheese!"
-	element.submit
+    expect(driver.title.downcase).to eq('网才')
+    driver.get 'http://ehire.51job.com/Candidate/SearchResumeIndex.aspx'
+    puts 'at search page'
 
-	wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-	wait.until { driver.title.downcase.start_with? "cheese!" }
+    puts 'filling filters'
+    sleep(2)
 
-	puts "Page title is #{driver.title}"
-    expect(driver.title.downcase).to include('cheese!')
+    ddlist = driver.find_element(:css, "#DpSearchList")
+    select_list = Selenium::WebDriver::Support::Select.new(ddlist)
+    select_list.select_by(:text, 'test0111')
 
-	driver.quit
+    sleep(2)
+
+
+    driver.find_element(:css, '#btnSearch').click
+
+    puts "clicked and wait"
+
+    sleep(5)
+
+    baseInfos = driver.find_elements(:xpath,"//tr[contains(@id,'trBaseInfo')]")
+    details = driver.find_elements(:xpath,"//tr[contains(@id,'trDetail')]")
+
+    infoWanted = []
+    5.times do |i|
+      curInfo = {}
+      re = baseInfos[i].text.scan /\s*(\S+)\s*/
+      curInfo[:id] = re[0][0]
+      curInfo[:exp] = re[1][0]
+      curInfo[:edu] = re[4][0]
+      a='最近工作：'
+      b='自我评价：'
+      re = details[i].text.scan /.*/
+      curInfo[:exjob] = re[0]
+      curInfo[:eval] = re[re.size - 2]
+      infoWanted << curInfo
+    end
+
+    puts infoWanted
+    driver.quit
+
+    #curl https://install.meteor.com/ | sh
   end
 end
 
