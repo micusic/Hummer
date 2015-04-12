bbfe5c8547c86fd186d5f419f66620718dcc724d
Micus Zhang <micus.zhang@gmail.com>
2015-04-12 15:50:47 +0800

    remove unused code

--- spec/hummer_spec.rb
+++ spec/hummer_spec.rb
@@ -5,8 +5,8 @@ require 'net/http'
 
 describe '51Job' do
   it 'get latest resumes' do
-    # driver = Selenium::WebDriver.for :phantomjs
-    driver = Selenium::WebDriver.for :chrome
+    driver = Selenium::WebDriver.for :phantomjs
+    # driver = Selenium::WebDriver.for :chrome
     driver.get 'http://ehire.51job.com/MainLogin.aspx'
     expect(driver.title.downcase).to eq('人才招聘-前程无忧 | 51job 网才')
     puts 'opened 51Job.'
